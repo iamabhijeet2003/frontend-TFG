aa9926d11b685307ae7f375aec18ef9b068a1f61
abhijeet <abhijeet3016@gmail.com>
2024-02-27 22:37:31 +0100

    adding cart components and its parts

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,10 @@
+<ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+    <li class="nav-item me-3">
+      <router-link class="nav-link" :class="$route.name == 'Cart'? 'active':''" aria-current="page" :to="{ name: 'Cart' }">
+        <i class="bi bi-cart3 h4"></i>
+        <span v-if="$store.state.cart.length > 0" class="align-items-center justify-content-center translate-middle badge rounded-pill bg-secondary">
+          {{ $store.state.cart.length }}
+        </span>
+      </router-link>
+    </li>
+</ul>
