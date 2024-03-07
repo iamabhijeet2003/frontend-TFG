451f5c013fabd9f9d2da882015f21277211cc68c
abhijeet <abhijeet3016@gmail.com>
2024-03-07 23:11:51 +0100

    add middleware and authentication for every route

--- src/router/index.js
+++ src/router/index.js
@@ -3,28 +3,42 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductsView from '../views/ProductsView.vue'
 import CartView from '../views/CartView.vue'
-import CheckoutView from '../views/CheckoutView.vue'
+import CheckoutView from '../views/CheckoutView.vue';
+import Login from "../views/LoginView.vue";
+//import store from '../store/';
+import { authMiddleware } from '../middleware/auth.js';
+
 const routes = [
     {
         path: '/',
         name: 'Home',
-        component: HomeView
+        component: HomeView,
+        meta: { requiresAuth: true }
     },
     {
         path: '/products',
         name: 'products',
-        component: ProductsView
+        component: ProductsView,
+        meta: { requiresAuth: true }
     },
     {
         path:'/cart',
         name:'Cart',
-        component:CartView
+        component:CartView,
+        meta: { requiresAuth: true }
     },
     {
         path: '/checkout',
         name:'Checkout',
-        component:CheckoutView
-    }
+        component:CheckoutView,
+        meta: { requiresAuth: true }
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login,
+      },
+      
 
 ]
 
@@ -32,5 +46,15 @@ const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes
 })
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth) {
+        // Apply authMiddleware to protected routes
+        authMiddleware(to, from, next);
+    } else {
+        // For non-protected routes, proceed to the next route
+        next();
+    }
+});
 
 export default router
