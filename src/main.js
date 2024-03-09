c5d4d7465f70726c7eca38a11d9028ae12dd362a
abhijeet <abhijeet3016@gmail.com>
2024-03-09 23:19:17 +0100

    import bootstrap icons

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ import router from './router'
 import store from "./store"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.js'
-
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
 
 createApp(App).use(bootstrap).use(router).use(store).mount('#app')
