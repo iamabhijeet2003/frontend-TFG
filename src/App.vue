23dc28c9b75bcc07b217868a89754ad4f7292761
abhijeet <abhijeet3016@gmail.com>
2024-03-09 23:18:49 +0100

    added vuex state management

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 <script>
 import NavBar from './components/NavBar.vue';
 import FooterPart from './components/partials/FooterPart.vue';
+import { mapGetters, mapActions } from 'vuex';
 export default {
   name: 'App',
   async mounted() {
@@ -24,7 +25,19 @@ export default {
       
     }
   },
-  
+  computed: {
+        ...mapGetters(['isLoggedIn']), // Map isLoggedIn getter
+    },
+    methods: {
+        ...mapActions(['checkAuthentication']), // Map checkAuthentication action
+        ...mapActions(['logout']),
+            handleLogout() {
+            this.logout(); // Call the logout action when the logout button is clicked
+            }
+    },
+    created() {
+        this.checkAuthentication(); // Check authentication status when the component is created
+    },
 };
 </script>
 
