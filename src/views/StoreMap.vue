fded60e7ac8d555d7255b7f14647e6cf591e4410
abhijeet <abhijeet3016@gmail.com>
2024-03-10 21:43:11 +0100

    add iframe of google maps

---
+++ src/views/StoreMap.vue
@@ -0,0 +1,20 @@
+<template>
+    <div  class="z-depth-1-half map-container">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d833.2981949837655!2d-0.2606403295700272!3d39.0775013034509!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd61c7636092196d%3A0x82082fc32f3032f7!2sIES%20Jaume%20II%20el%20Just!5e1!3m2!1sen!2ses!4v1710092057719!5m2!1sen!2ses"
+        width="600"
+        height="450"
+        style="border:0;"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'StoreMap',
+  };
+  </script>
+  
