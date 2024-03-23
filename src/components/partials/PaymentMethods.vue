218af45120c8c63abecf7a7449a8c33fbaf964fa
abhijeet <abhijeet3016@gmail.com>
2024-03-23 23:59:09 +0100

    adding more bootstrap sizes

--- src/components/partials/PaymentMethods.vue
+++ src/components/partials/PaymentMethods.vue
@@ -1,34 +1,33 @@
 <template>
-     <link
-    href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css"
-    rel="stylesheet"
-/>
-<h2 class="h2 fw-bold text-center my-5">Metodos de Pago</h2>
-    <div class="container mx-5 mx-auto px-3">   
-      <div class="text-center d-flex justify-content-evenly fs-1">
-        <i class="bi bi-paypal "></i>
-        <i class="bi bi-stripe"></i>
-        <i class="bi bi-alipay"></i>
-        <i class="bi bi-credit-card"></i>
-        <i class="bi bi-wallet"></i>
-        <i class="bi bi-bank"></i>
-        <i class="bi bi-currency-euro"></i>
-        <i class="bi bi-currency-dollar"></i>
-        <i class="bi bi-currency-bitcoin"></i>
-        <i class="bi bi-currency-pound"></i>
-        <i class="bi bi-apple"></i>
-        <i class="bi bi-google"></i>
-        <i class="ri-mastercard-fill"></i>
-        <i class="ri-visa-fill"></i>
+  <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet" />
+  <h2 class="h2 fw-bold text-center my-5">Metodos de Pago</h2>
+  <div class="container mx-5 mx-auto px-3">
+    <div class="row justify-content-center">
+      <div class="col">
+        <div class="text-center d-flex justify-content-evenly fs-1">
+          <i class="bi bi-paypal "></i>
+          <i class="bi bi-stripe"></i>
+          <i class="bi bi-alipay"></i>
+          <i class="bi bi-credit-card"></i>
+          <i class="bi bi-wallet"></i>
+          <i class="bi bi-bank"></i>
+          <i class="bi bi-currency-euro"></i>
+          <i class="bi bi-currency-dollar"></i>
+          <i class="bi bi-currency-bitcoin"></i>
+          <i class="bi bi-currency-pound"></i>
+          <i class="bi bi-apple"></i>
+          <i class="bi bi-google"></i>
+          <i class="ri-mastercard-fill"></i>
+          <i class="ri-visa-fill"></i>
+        </div>
       </div>
     </div>
-  </template>
-  
-  <script>
- 
-  export default {
-    name: 'PaymentMethods',
-  };
-  </script>
+  </div>
+</template>
 
-  
+<script>
+
+export default {
+  name: 'PaymentMethods',
+};
+</script>
