83ca0493a05f24a6c600eabfa26010d4b082f46b
abhijeet <abhijeet3016@gmail.com>
2024-03-24 00:00:39 +0100

    adding vuex state and making data persistent over pages

--- src/store/index.js
+++ src/store/index.js
@@ -1,66 +1,68 @@
 // frontend/src/store/index.js
-import { createStore } from 'vuex'
+
+import { createStore } from 'vuex';
+import createPersistedState from "vuex-persistedstate";
 /* eslint-disable */
 export default createStore({
   state: {
-    cart:[],
-    cartTotal:0,
+    cart: [],
+    cartTotal: 0,
     user: null,
     token: null,
     isAuthenticated: false,
   },
   mutations: {
 
-   async initialiseStore(state) {
-         if(localStorage.getItem('cart')){
-          state.cart = JSON.parse(localStorage.getItem('cart'))
-         }
-         if(localStorage.getItem('cartTotal')){
-         state.cartTotal = parseFloat(localStorage.getItem('cartTotal')) 
-         }
-         return true;
+    async initialiseStore(state) {
+      if (localStorage.getItem('cart')) {
+        state.cart = JSON.parse(localStorage.getItem('cart'))
+      }
+      if (localStorage.getItem('cartTotal')) {
+        state.cartTotal = parseFloat(localStorage.getItem('cartTotal'))
+      }
+      return true;
     },
-    addRemoveCart(state,payload){
+    addRemoveCart(state, payload) {
 
       //add or remove item
-      payload.toAdd? 
-      state.cart.push(payload.product):
-      state.cart = state.cart.filter(function( obj ) {
-        return obj.id !== payload.product.id;
-      });
+      payload.toAdd ?
+        state.cart.push(payload.product) :
+        state.cart = state.cart.filter(function (obj) {
+          return obj.id !== payload.product.id;
+        });
 
       //calculating the total
       state.cartTotal = state.cart.reduce((accumulator, object) => {
-   
-        return parseFloat(accumulator) + parseFloat(object.price*object.qty);
+
+        return parseFloat(accumulator) + parseFloat(object.price * object.qty);
       }, 0);
 
-       //saving in web storage
-      localStorage.setItem('cartTotal',JSON.stringify(state.cartTotal));
-      localStorage.setItem('cart',JSON.stringify(state.cart));
+      //saving in web storage
+      localStorage.setItem('cartTotal', JSON.stringify(state.cartTotal));
+      localStorage.setItem('cart', JSON.stringify(state.cart));
     },
-    updateCart(state,payload){
-         //update quantity 
-        state.cart.find(o => o.id ===  payload.product.id).qty = payload.product.qty;
+    updateCart(state, payload) {
+      //update quantity 
+      state.cart.find(o => o.id === payload.product.id).qty = payload.product.qty;
 
-       //calculating the total
-        state.cartTotal = state.cart.reduce((accumulator, object) => {
-          return parseFloat(accumulator) + parseFloat(object.price*object.qty);
-        }, 0);
+      //calculating the total
+      state.cartTotal = state.cart.reduce((accumulator, object) => {
+        return parseFloat(accumulator) + parseFloat(object.price * object.qty);
+      }, 0);
 
       //saving in web storage
-        localStorage.setItem('cartTotal',JSON.stringify(state.cartTotal));
-        localStorage.setItem('cart',JSON.stringify(state.cart));
-      },
-      setUser(state, user) {
-        state.user = user;
-      },
-      setToken(state, token) {
-        state.token = token;
-      },
-      setAuthentication(state, isAuthenticated) {
-        state.isAuthenticated = isAuthenticated;
-      },
+      localStorage.setItem('cartTotal', JSON.stringify(state.cartTotal));
+      localStorage.setItem('cart', JSON.stringify(state.cart));
+    },
+    setUser(state, user) {
+      state.user = user;
+    },
+    setToken(state, token) {
+      state.token = token;
+    },
+    setAuthentication(state, isAuthenticated) {
+      state.isAuthenticated = isAuthenticated;
+    },
   },
   actions: {
     checkAuthentication({ commit }) {
@@ -76,13 +78,27 @@ export default createStore({
       commit('setToken', null); // Clear token in Vuex store
       commit('setAuthentication', false); // Set authentication status to false
       commit('setUser', null); // Clear user data
-      location.reload(); 
-    }
+      location.reload();
+    },
+    async fetchUserProfile({ commit }) {
+      try {
+        const response = await fetch('http://localhost:8000/profile', {
+          headers: {
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+          }
+        });
+        const userData = await response.json();
+        commit('setUser', userData);
+      } catch (error) {
+        console.error('Error fetching user profile:', error);
+      }
+    },
   },
   modules: {},
   getters: {
     isLoggedIn(state) {
       return !!state.token;
     },
-  }
+  },
+  plugins: [createPersistedState()],
 })
