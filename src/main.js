91ea28a3145b3173ccc35bcd48cd641ca76657c5
abhijeet <abhijeet3016@gmail.com>
2024-03-31 23:46:25 +0200

    add vuestic buefy tailwindcss

--- src/main.js
+++ src/main.js
@@ -6,5 +6,15 @@ import store from "./store"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
+import { createVuestic } from "vuestic-ui";
+import "vuestic-ui/css";
+import Buefy from '@ntohq/buefy-next';
+import './index.css'
 
-createApp(App).use(bootstrap).use(router).use(store).mount('#app')
+createApp(App)
+    .use(bootstrap)
+    .use(router)
+    .use(store)
+    .use(createVuestic())
+    .use(Buefy)
+    .mount('#app')
