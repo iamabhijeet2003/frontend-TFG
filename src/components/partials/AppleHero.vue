64cb52361d7772169986c49707b9a57e9383d4e2
abhijeet <abhijeet3016@gmail.com>
2024-04-01 13:18:51 +0200

    add hero banner of iphone 15

---
+++ src/components/partials/AppleHero.vue
@@ -0,0 +1,16 @@
+<template>
+    <a href="/product/1">
+        <section class="tw-cursor-pointer tw-text-center">
+            <h1 class="tw-text-6xl tw-mt-8 tw-font-bold tw-font-semibold">
+                iPhone 15 Pro
+            </h1>
+            <p class="tw-text-3xl tw-my-2 tw-opacity-90">
+                A total powerhouse.
+            </p>
+            <span class="tw-flex tw-justify-center tw-space-x-10 tw-text-xl">
+                <p class="tw-text-blue-600 tw-hover:tw-underline"> Buy > </p>
+            </span>
+            <img class="tw-mx-auto tw-my-3" src="https://res.cloudinary.com/dcm40dfr5/image/upload/v1711970233/cp1_ve9ewq.png" alt="iphone-13-pro">
+        </section>
+    </a>
+</template>
