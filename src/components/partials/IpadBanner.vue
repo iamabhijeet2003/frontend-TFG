dfb96ad9beb2c8324a9f289efde9e64ceb86bc65
abhijeet <abhijeet3016@gmail.com>
2024-04-01 14:02:17 +0200

    add ipad banner

---
+++ src/components/partials/IpadBanner.vue
@@ -0,0 +1,25 @@
+<template>
+    <a href="/product/3">
+        <section class="tw-bg-black tw-flex tw-py-10 tw-mt-20">
+            <div class="tw-text-white tw-flex tw-ml-36 tw-my-auto tw-items-center tw-flex-col">
+                <div class="tw-flex tw-flex-row tw-mx-auto">
+                    <h1 class="tw-text-5xl tw-font-semibold tw-mt-3">iPad</h1>
+                    <img class="" src="https://res.cloudinary.com/dcm40dfr5/image/upload/v1711970479/c3_h4kefw.png" alt="ipad">
+                </div>
+                <div class="tw-flex tw-flex-col tw-mx-auto">
+                    <p class="tw-text-3xl tw-font-semibold">Light. Bright. Full of might.</p>
+                    <span class="tw-flex tw-mx-auto tw-my-3 tw-text-lg">
+                        <p class="tw-text-blue-600 tw-hover:underline tw-ml-4 tw-mr-1 tw-font-semibold tw-text-xl">Buy
+                        </p>
+                        <p class="tw-text-blue-600">></p>
+                    </span>
+                </div>
+            </div>
+
+            <div class="tw-my-auto tw-flex tw-mx-auto">
+                <img class="tw-h-full tw-flex tw-justify-center tw-items-center"
+                    src="https://res.cloudinary.com/dcm40dfr5/image/upload/v1711970406/c4_vnxipj.png" alt="iPad">
+            </div>
+        </section>
+    </a>
+</template>
