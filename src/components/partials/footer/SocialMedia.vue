78cb26a41fddc2a444e90d6b46fba03d8477f909
abhijeet <abhijeet3016@gmail.com>
2024-04-04 23:57:38 +0200

    add icons in new component - code level splitting

---
+++ src/components/partials/footer/SocialMedia.vue
@@ -0,0 +1,45 @@
+<template>
+     <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+              <!-- Facebook -->
+              <a class="btn btn-outline-light m-1 text-white" role="button" aria-label="facebook"><i
+                  class="bi bi-facebook fs-5"></i></a>
+
+
+              <!-- Twitter -->
+              <a class="btn btn-outline-light btn-floating m-1 text-white" role="button" aria-label="twitter"><i
+                  class="bi bi-twitter-x fs-5"></i></a>
+
+              <!-- Google -->
+              <a class="btn btn-outline-light btn-floating m-1 text-white" role="button" aria-label="google"><i
+                  class="bi bi-google fs-5"></i></a>
+
+              <!-- Instagram -->
+              <a class="btn btn-outline-light btn-floating m-1 text-white" role="button" aria-label="instagram"><i
+                  class="bi bi-instagram fs-5"></i></a>
+
+              <!-- Linkedin -->
+              <a class="btn btn-outline-light btn-floating m-1 text-white" role="button" aria-label="linkedin"><i
+                  class="bi bi-linkedin fs-5"></i></a>
+
+              <!-- whatsapp -->
+              <a class="btn btn-outline-light btn-floating m-1 text-white" role="button" aria-label="whatsaap"><i
+                  class="bi bi-whatsapp fs-5"></i></a>
+
+              <!-- telegram -->
+              <a class="btn btn-outline-light btn-floating m-1 text-white" role="button" aria-label="telegram"><i
+                  class="bi bi-telegram fs-5"></i></a>
+
+              <!-- youtube -->
+              <a class="btn btn-outline-light btn-floating m-1 text-white" role="button" aria-label="youtube"><i
+                  class="bi bi-youtube fs-5"></i></a>
+
+              <!-- pinterest -->
+              <a class="btn btn-outline-light btn-floating m-1 text-white" role="button" aria-label="pinterest"><i
+                  class="bi bi-pinterest fs-5"></i></a>
+
+              <!-- github -->
+              <a class="btn btn-outline-light btn-floating m-1 text-white" role="button" aria-label="github" href="https://github.com/iamabhijeet2003" ><i
+                  class="bi bi-github fs-5"></i></a>
+                
+            </div>
+</template>
