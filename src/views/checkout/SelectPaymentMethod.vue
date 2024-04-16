989cdcb8713b2c67a3504fa7e312dfbe0e4dfc07
abhijeet <abhijeet3016@gmail.com>
2024-04-16 23:57:59 +0200

    add option to select the payment method

---
+++ src/views/checkout/SelectPaymentMethod.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1>Select the payment method</h1>
+
+    <router-link to="/checkout-card">Pay with card</router-link>
+    <router-link to="/checkout">COD</router-link>
+</template>
