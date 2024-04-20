84a5bb07eb4be27a8865835cbb7d32a006782c40
abhijeet <abhijeet3016@gmail.com>
2024-04-20 10:37:01 +0200

    add layouts

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,19 @@
+<!-- AuthLayout.vue -->
+<template>
+    <div class="auth-layout">
+      <router-view /> <!-- Render child routes -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Add any specific logic or styles for the authentication layout
+  };
+  </script>
+  
+  <style scoped>
+  /* .auth-layout {
+    
+  } */
+  </style>
+  
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,25 @@
+<!-- DefaultLayout.vue -->
+<template>
+    <div>
+      <NavBar /> <!-- Include navigation bar -->
+      <router-view /> <!-- Render child routes -->
+      <FooterPart /> <!-- Include footer -->
+    </div>
+  </template>
+  
+  <script>
+  import NavBar from '@/components/NavBar.vue'; // Import navigation bar component
+  import FooterPart from '@/components/partials/FooterPart.vue'; // Import footer component
+  
+  export default {
+    components: {
+      NavBar,
+      FooterPart,
+    },
+  };
+  </script>
+  
+  <style>
+  /* Add custom styles here */
+  </style>
+  
