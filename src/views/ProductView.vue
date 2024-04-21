1daebbb3bfe11e7bb8696598af6e734919697486
abhijeet <abhijeet3016@gmail.com>
2024-04-21 23:48:38 +0200

    adding style and rating component

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -2,21 +2,27 @@
     <div class="container">
         <div v-if="loading">Loading...</div>
         <div v-else>
-            <div class="col-md-6">
-                <img :src="product.image" alt="Product Image" class="img-fluid" style="max-width: 400px;">
-            </div>
-            <h1>{{ product.name }}</h1>
-            <p>{{ product.description }}</p>
-            <p>Price: ${{ product.price }}</p>
+            
+            <div class="tw-rounded-lg tw-bg-gradient-to-tr tw-from-pink-300 tw-to-blue-300 tw-p-0.5 tw-shadow-lg mt-4">
+                <div class="tw-bg-white tw-p-7 tw-rounded-md"> 
+                    
+                    <div class="col-md-6 text-center mx-auto">
+                        <img :src="product.image" alt="Product Image" class="img-fluid mx-auto" style="max-width: 400px;">
+                    </div>
+                    <h1  class="tw-text-transparent tw-bg-clip-text tw-bg-gradient-to-tr tw-to-cyan-500 tw-from-blue-600 fs-1 h1 fw-bolder text-center">{{ product.name }}</h1>
+                    <p class="tw-text-transparent tw-bg-clip-text tw-bg-gradient-to-tr tw-to-cyan-500 tw-from-blue-600 fs-3 fw-bold text-center">{{ product.description }}</p>
+                    <p class="tw-text-transparent tw-bg-clip-text tw-bg-gradient-to-tr tw-to-cyan-500 tw-from-blue-600 fs-3 fw-bold text-center">Price: ${{ product.price }}</p>
 
-            <div>
-                <h3>Add to Cart</h3>
-                <div class="btn-group">
+                    <div class="text-center mx-auto">
+                        <h3 class="tw-text-transparent tw-bg-clip-text tw-bg-gradient-to-tr tw-to-cyan-500 tw-from-blue-600 fs-3 h3 fw-bolder text-center">Add to Cart</h3>
+                        <div class="btn-group align-center mx-auto text-center">
 
-                    <CartBTN :product="product" aria-label="Add to cart" />
+                            <CartBTN :product="product" aria-label="Add to cart" />
+                        </div>
+                    </div>
                 </div>
             </div>
-            <!-- <ProductRatings :productId="product.id" /> -->
+            <ProductRatings :productId="product.id" />
 
             <!-- <RatingForm :productId="product.id" /> -->
         </div>
@@ -26,7 +32,7 @@
 <script>
 import axios from 'axios';
 import { API_ROOT_URL } from '@/apiConfig';
-// import ProductRatings from '../components/ratings/ProductRating.vue';
+import ProductRatings from '../components/ratings/ProductRating.vue';
 import CartBTN from '../components/CartBTN.vue';
 // import RatingForm from '../components/ratings/AddRating.vue'; 
 
@@ -38,7 +44,7 @@ export default {
         };
     },
     components: {
-        // ProductRatings,
+        ProductRatings,
         CartBTN,
         // RatingForm,
     },
@@ -72,5 +78,5 @@ export default {
 </script>
 
 <style scoped>
-/* Add your styles here */
+
 </style>
