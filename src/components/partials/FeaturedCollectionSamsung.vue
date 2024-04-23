6aad8dffd0c9ea3b7faae6385ab5234e32b46276
abhijeet <abhijeet3016@gmail.com>
2024-04-23 21:30:12 +0200

    add i18n and router link

--- src/components/partials/FeaturedCollectionSamsung.vue
+++ src/components/partials/FeaturedCollectionSamsung.vue
@@ -1,18 +1,32 @@
 <template>
     <section class="tw-bg-white tw-dark:bg-gray-900">
-        <div class="tw-grid tw-max-w-screen-xl tw-px-4 tw-py-8 tw-mx-auto lg:tw-gap-8 xl:tw-gap-0 lg:tw-py-16 lg:tw-grid-cols-12">
+        <div
+            class="tw-grid tw-max-w-screen-xl tw-px-4 tw-py-8 tw-mx-auto lg:tw-gap-8 xl:tw-gap-0 lg:tw-py-16 lg:tw-grid-cols-12">
             <div class="tw-mr-auto tw-place-self-center lg:tw-col-span-7">
-                <h1 class="tw-max-w-2xl tw-mb-4 tw-text-4xl tw-font-extrabold tw-tracking-tight tw-leading-none md:tw-text-5xl xl:tw-text-6xl tw-dark:text-white">Explore the latest Samsung products</h1>
-                <p class="tw-max-w-2xl tw-mb-6 tw-font-light tw-text-gray-500 lg:tw-mb-8 md:tw-text-lg lg:tw-text-xl tw-dark:text-gray-400">Discover the newest offerings from Samsung and stay ahead with cutting-edge technology.</p>
-                <a href="/products/samsung" class="tw-inline-flex tw-items-center tw-justify-center tw-px-5 tw-py-3 tw-mr-3 tw-text-base tw-font-medium tw-text-center tw-text-black tw-rounded-lg tw-bg-primary-700 tw-hover:bg-primary-800 tw-focus:tw-ring-4 tw-focus:tw-ring-primary-300 tw-dark:focus:tw-ring-primary-900">
-                    Explore Now
-                    <svg class="tw-w-5 tw-h-5 tw-ml-2 tw--mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                <h1
+                    class="tw-max-w-2xl tw-mb-4 tw-text-4xl tw-font-extrabold tw-tracking-tight tw-leading-none md:tw-text-5xl xl:tw-text-6xl tw-dark:text-white">
+                    {{ $t('samsung_featured.exploreLatestProducts') }}
+                </h1>
+                <p
+                    class="tw-max-w-2xl tw-mb-6 tw-font-light tw-text-gray-500 lg:tw-mb-8 md:tw-text-lg lg:tw-text-xl tw-dark:text-gray-400">
+                    {{ $t('samsung_featured.discoverNewestOfferings') }}
+                </p>
+                <a href="/products?brand=2"
+                    class="tw-inline-flex tw-items-center tw-justify-center tw-px-5 tw-py-3 tw-mr-3 tw-text-base tw-font-medium tw-text-center tw-text-black tw-rounded-lg tw-bg-primary-700 tw-hover:bg-primary-800 tw-focus:tw-ring-4 tw-focus:tw-ring-primary-300 tw-dark:focus:tw-ring-primary-900">
+                    {{ $t('samsung_featured.exploreNow') }}
+                    <svg class="tw-w-5 tw-h-5 tw-ml-2 tw--mr-1" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                            clip-rule="evenodd"></path>
+                    </svg>
                 </a>
-              
+
             </div>
             <div class="tw-hidden lg:tw-mt-0 lg:tw-col-span-5 lg:tw-flex">
-                <img src="https://images.samsung.com/is/image/samsung/p6pim/es/2401/gallery/es-galaxy-s24-s928-491123-sm-s928bztheub-539444345?$684_547_JPG$" alt="mockup">
-            </div>                
+                <img src="https://images.samsung.com/is/image/samsung/p6pim/es/2401/gallery/es-galaxy-s24-s928-491123-sm-s928bztheub-539444345?$684_547_JPG$"
+                    alt="mockup">
+            </div>
         </div>
     </section>
 </template>
