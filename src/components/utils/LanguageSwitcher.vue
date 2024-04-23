ef39df47e12c90a39cbfdb3c0357884911eab12a
abhijeet <abhijeet3016@gmail.com>
2024-04-23 21:32:39 +0200

    add new style

--- src/components/utils/LanguageSwitcher.vue
+++ src/components/utils/LanguageSwitcher.vue
@@ -1,6 +1,6 @@
 <!-- /src/components/utils/LanguageSwitcher.vue -->
 <template>
-    <select class="App-language form-select" v-model="locale">
+    <select class="App-language form-select" v-model="locale">     
       <option
         v-for="optionLocale in supportLocales"
         :key="`locale-${optionLocale}`"
@@ -18,3 +18,14 @@ watch(locale, (val) => {
   setI18nLanguage(val);
 });
 </script>
+<style scoped>
+select {
+	padding: 0.2em  0.5em 0.2em 1.5em;
+	background-image: url( https://static-00.iconduck.com/assets.00/globe-icon-2048x2048-5ralwwgx.png );
+	background-repeat: no-repeat;
+	background-size: 0.8em auto;
+	background-position: 0.4em center;
+	font-size: 1.5em;
+  color: black;
+ }
+ </style>
