b98818d5fef02d6998b9882bc17d6d4cde5ac17c
abhijeet <abhijeet3016@gmail.com>
2024-04-23 21:31:30 +0200

    add new style

--- src/components/ratings/AddRating.vue
+++ src/components/ratings/AddRating.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="rating-form">
-    <h3>Add Rating</h3>
-    <form @submit.prevent="submitRating">
-      <div class="form-group">
-        <label for="rating">Rating:</label>
-        <input type="number" v-model="rating" id="rating" class="form-control" min="1" max="5" required>
-      </div>
-      <div class="form-group">
-        <label for="comment">Comment:</label>
-        <textarea v-model="comment" id="comment" class="form-control" rows="3"></textarea>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+   <div class="product-ratings tw-relative tw-mx-auto tw-rounded-lg tw-bg-gradient-to-tr tw-from-pink-300 tw-to-blue-300 tw-p-0.5 tw-shadow-lg">
+    <div class="tw-bg-white tw-p-7 tw-rounded-md">
+    <div class="rating-form">
+      <h3 class="tw-text-4xl tw-font-extrabold tw-text-transparent tw-bg-clip-text tw-bg-gradient-to-r tw-from-rose-600 tw-via-red-400 tw-to-orange-500 h3 fw-bold text-center fs-1 mb-1" >Add Rating</h3>
+      <form @submit.prevent="submitRating">
+        <div class="form-group">
+          <label for="rating">Rating:</label>
+          <input type="number" v-model="rating" id="rating" class="form-control" min="1" max="5" required>
+        </div>
+        <div class="form-group">
+          <label for="comment">Comment:</label>
+          <textarea v-model="comment" id="comment" class="form-control" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+    </div>
   </div>
 </template>
 
@@ -30,10 +34,10 @@ export default {
       try {
         const token = localStorage.getItem('token');
         const userid = localStorage.getItem('user_id'); // Get user ID from localStorage
-        const productid= this.productId;
+        const productid = this.productId;
         const response = await axios.post('http://localhost:8000/api/ratingss', {
           productId: `/api/products/${productid}`,
-          userId:`/api/users/${userid}`,
+          userId: `/api/users/${userid}`,
           ratingValue: this.rating,
           comment: this.comment,
         }, {
