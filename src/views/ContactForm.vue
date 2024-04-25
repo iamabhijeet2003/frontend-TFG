6b7b7f5d0fccfb051cea72357ffde65b60649e55
abhijeet <abhijeet3016@gmail.com>
2024-04-25 23:46:15 +0200

    make post to the contact api

--- src/views/ContactForm.vue
+++ src/views/ContactForm.vue
@@ -26,14 +26,14 @@
                             <textarea v-model="formData.message" name="Message" id="message" rows="4" required
                                 class="form-control"></textarea>
                         </div>
-                        <button style="margin: 1rem 0" @click="sendExample" class="btn btn-primary btn-lg">
+                        <button style="margin: 1rem 0" @click="submitForm()" class="btn btn-primary btn-lg">
                             Send test data
                         </button>
                     </form>
                 </div>
-                
+
             </div>
-            
+
             <div class="col-6 my-4 p-4">
                 <h1 class="h1 text-center">Nuestra Tienda Fisica</h1>
                 <StoreMap></StoreMap>
@@ -44,7 +44,9 @@
 
 <script>
 import StoreMap from "./StoreMap.vue";
-import UseEmail from "./UseEmail";
+//import UseEmail from "./UseEmail";
+import axios from "axios";
+import Swal from 'sweetalert2';
 export default {
     name: 'ContactForm',
     data() {
@@ -57,26 +59,58 @@ export default {
             },
         };
     },
-    components:{
+    components: {
         StoreMap,
     },
     methods: {
         submitForm() {
-            // You can handle form submission logic here
+            const { name, email, phone, message } = this.formData;
+
+            const phoneNumber = parseInt(phone);
+
+            const requestData = {
+                name,
+                email,
+                phone: phoneNumber,
+                message
+            };
+            const token = localStorage.getItem("token");
+            axios.post('http://127.0.0.1:8000/api/contacts', requestData,{
+                headers:{
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/ld+json'
+                }
+            })
+                .then(response => {
+                    console.log('Form submitted successfully:', response.data);
+                    
+                    this.resetForm();
+                    // Display success message using Swal
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Your message has been submitted successfully.',
+                        icon: 'success',
+                        confirmButtonText: 'OK',
+                    });
+                })
+                .catch(error => {
+                    console.error('Error submitting form:', error);
+                    Swal.fire({
+                        title: 'Error!',
+                        text: 'An error occurred while submitting your message.',
+                        icon: 'error',
+                        confirmButtonText: 'OK',
+                    });
+                });
             console.log('Form submitted:', this.formData);
         },
-        sendExample() {
-            const { name, email, phone, message } = this.formData;
-            UseEmail('https://public.herotofu.com/v1/33d7c2c0-df06-11ee-8980-3397320e035b').sendEmail({
-                example_user: email,
-                example_data: {
-                    name,
-                    email,
-                    phone,
-                    message
-                },
-            });
-        },
+        resetForm() {
+            // Reset form data after successful submission
+            this.formData.name = '';
+            this.formData.email = '';
+            this.formData.phone = '';
+            this.formData.message = '';
+        }
     },
 };
 </script>
