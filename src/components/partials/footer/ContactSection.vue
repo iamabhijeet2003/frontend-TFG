9d13268f7a32f1033b0df1f517d27229e6d805a4
abhijeet <abhijeet3016@gmail.com>
2024-04-26 11:13:06 +0200

    add translations

--- src/components/partials/footer/ContactSection.vue
+++ src/components/partials/footer/ContactSection.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-        <h6 class="text-uppercase mb-4 font-weight-bold">Contacto</h6>
-        <p><i class="bi bi-house-door fs-6"></i> Cami La Dula S/N I.E.S Jaume II El Just</p>
-        <p><i class="bi bi-envelope fs-6"></i> abhijeet3016@gmail.com</p>
-        <p><i class="bi bi-telephone-fill fs-6"></i> + 34 000 000 66</p>
-        <p><i class="bi bi-printer fs-6"></i> + 34 000 000 66</p>
+        <h6 class="text-uppercase mb-4 font-weight-bold">{{ $t('footer.contact') }}</h6>
+        <p><i class="bi bi-house-door fs-6"></i> {{ $t('footer.address') }} </p>
+        <p><i class="bi bi-envelope fs-6"></i> {{ $t('footer.email') }}</p>
+        <p><i class="bi bi-telephone-fill fs-6"></i>{{ $t('footer.phone') }}</p>
+        <p><i class="bi bi-printer fs-6"></i>{{ $t('footer.fax') }}</p>
     </div>
 </template>
