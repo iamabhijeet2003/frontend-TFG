14c6a1c289c59d237aa92c3715f2b4dbe26fdbe2
abhijeet <abhijeet3016@gmail.com>
2024-04-26 11:02:51 +0200

    add translations

--- src/components/partials/AppleHero.vue
+++ src/components/partials/AppleHero.vue
@@ -3,13 +3,13 @@
     <a href="/product/1">
         <section class="tw-cursor-pointer tw-text-center">
             <h1 class="tw-text-6xl tw-mt-8 tw-font-bold tw-font-semibold">
-                iPhone 15 Pro
+                {{ $t('apple_hero.title') }}
             </h1>
             <p class="tw-text-3xl tw-my-2 tw-opacity-90">
-                A total powerhouse.
+                {{ $t('apple_hero.description') }}
             </p>
             <span class="tw-flex tw-justify-center tw-space-x-10 tw-text-xl">
-                <p class="tw-text-blue-600 tw-hover:tw-underline"> Buy > </p>
+                <p class="tw-text-blue-600 tw-hover:tw-underline"> {{ $t('apple_hero.buy_now') }} </p>
             </span>
             <img class="tw-mx-auto tw-my-3" src="https://res.cloudinary.com/dcm40dfr5/image/upload/v1711970233/cp1_ve9ewq.png" alt="iphone-13-pro">
         </section>
