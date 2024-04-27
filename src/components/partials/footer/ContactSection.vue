a4b80c0868952e267949138f32df312905242be2
abhijeet <abhijeet3016@gmail.com>
2024-04-27 21:29:14 +0200

    add email

--- src/components/partials/footer/ContactSection.vue
+++ src/components/partials/footer/ContactSection.vue
@@ -2,7 +2,7 @@
     <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
         <h6 class="text-uppercase mb-4 font-weight-bold">{{ $t('footer.contact') }}</h6>
         <p><i class="bi bi-house-door fs-6"></i> {{ $t('footer.address') }} </p>
-        <p><i class="bi bi-envelope fs-6"></i> {{ $t('footer.email') }}</p>
+        <p><i class="bi bi-envelope fs-6"></i> abhijeet3016@gmail.com </p>
         <p><i class="bi bi-telephone-fill fs-6"></i>{{ $t('footer.phone') }}</p>
         <p><i class="bi bi-printer fs-6"></i>{{ $t('footer.fax') }}</p>
     </div>
