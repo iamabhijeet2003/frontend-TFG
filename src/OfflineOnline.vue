cf0116d72631bf86bb4169f10274267b68e22b63
abhijeet <abhijeet3016@gmail.com>
2024-04-28 00:04:33 +0200

    add swal on offline

--- src/OfflineOnline.vue
+++ src/OfflineOnline.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue';
 import { toast } from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
+import Swal from 'sweetalert2';
 export default {
   setup() {
     const isOnline = ref(navigator.onLine);
@@ -15,6 +16,12 @@ export default {
     window.addEventListener('offline', () => {
       isOnline.value = false;
       toast.error('You are offline.');
+      Swal.fire({
+        title: 'You are offline!',
+        text: 'Please connect to the internet to see the products.',
+        icon: 'warning',
+        confirmButtonText: 'Ok'
+      });
     });
 
     return { isOnline };
