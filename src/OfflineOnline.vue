292b8d568870b8a1403d80884e534ff25c16b8b4
abhijeet <abhijeet3016@gmail.com>
2024-04-27 22:01:32 +0200

    add online offile functionality

---
+++ src/OfflineOnline.vue
@@ -0,0 +1,23 @@
+<script>
+import { ref } from 'vue';
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+export default {
+  setup() {
+    const isOnline = ref(navigator.onLine);
+
+    window.addEventListener('online', () => {
+      isOnline.value = true;
+      // Optionally display a success toast
+      toast.success('You are back online!');
+    });
+
+    window.addEventListener('offline', () => {
+      isOnline.value = false;
+      toast.error('You are offline.');
+    });
+
+    return { isOnline };
+  }
+};
+</script>
