4c99477fc1031b42c359541096e5ce8d7199e0e2
abhijeet <abhijeet3016@gmail.com>
2024-04-27 21:35:51 +0200

    add rating list and rating form, add styles

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -2,19 +2,29 @@
     <div class="container">
         <div v-if="loading">Loading...</div>
         <div v-else>
-            
-            <div class="tw-rounded-lg tw-bg-gradient-to-tr tw-from-pink-300 tw-to-blue-300 tw-p-0.5 tw-shadow-lg mt-4">
-                <div class="tw-bg-white tw-p-7 tw-rounded-md"> 
-                    
+
+            <div
+                class="tw-rounded-lg tw-bg-gradient-to-tr tw-from-pink-300 tw-to-blue-300 tw-p-0.5 tw-shadow-lg mt-4 mb-3">
+                <div class="tw-bg-white tw-p-7 tw-rounded-md">
+
                     <div class="col-md-6 text-center mx-auto">
-                        <img :src="product.image" alt="Product Image" class="img-fluid mx-auto" style="max-width: 400px;">
+                        <img :src="product.image" alt="Product Image" class="img-fluid mx-auto"
+                            style="max-width: 400px;">
                     </div>
-                    <h1  class="tw-text-transparent tw-bg-clip-text tw-bg-gradient-to-tr tw-to-cyan-500 tw-from-blue-600 fs-1 h1 fw-bolder text-center">{{ product.name }}</h1>
-                    <p class="tw-text-transparent tw-bg-clip-text tw-bg-gradient-to-tr tw-to-cyan-500 tw-from-blue-600 fs-3 fw-bold text-center">{{ product.description }}</p>
-                    <p class="tw-text-transparent tw-bg-clip-text tw-bg-gradient-to-tr tw-to-cyan-500 tw-from-blue-600 fs-3 fw-bold text-center">Price: ${{ product.price }}</p>
+                    <h1
+                        class="tw-text-transparent tw-bg-clip-text tw-bg-gradient-to-tr tw-to-cyan-500 tw-from-blue-600 fs-1 h1 fw-bolder text-center">
+                        {{ product.name }}</h1>
+                    <p
+                        class="tw-text-transparent tw-bg-clip-text tw-bg-gradient-to-tr tw-to-cyan-500 tw-from-blue-600 fs-3 fw-bold text-center">
+                        {{ product.description }}</p>
+                    <p
+                        class="tw-text-transparent tw-bg-clip-text tw-bg-gradient-to-tr tw-to-cyan-500 tw-from-blue-600 fs-3 fw-bold text-center">
+                        Price: ${{ product.price }}</p>
 
                     <div class="text-center mx-auto">
-                        <h3 class="tw-text-transparent tw-bg-clip-text tw-bg-gradient-to-tr tw-to-cyan-500 tw-from-blue-600 fs-3 h3 fw-bolder text-center">Add to Cart</h3>
+                        <h3
+                            class="tw-text-transparent tw-bg-clip-text tw-bg-gradient-to-tr tw-to-cyan-500 tw-from-blue-600 fs-3 h3 fw-bolder text-center">
+                            Add to Cart</h3>
                         <div class="btn-group align-center mx-auto text-center">
 
                             <CartBTN :product="product" aria-label="Add to cart" />
@@ -22,9 +32,13 @@
                     </div>
                 </div>
             </div>
-            <ProductRatings :productId="product.id" />
 
-            <RatingForm :productId="product.id" />
+            <div class="mt-3">
+                <RatingForm :productId="product.id" />
+            </div>
+            <div class="mt-3">
+                <ProductRatings :productId="product.id" />
+            </div>
         </div>
     </div>
 </template>
@@ -34,7 +48,7 @@ import axios from 'axios';
 import { API_ROOT_URL } from '@/apiConfig';
 import ProductRatings from '../components/ratings/ProductRating.vue';
 import CartBTN from '../components/CartBTN.vue';
-import RatingForm from '../components/ratings/AddRating.vue'; 
+import RatingForm from '../components/ratings/AddRating.vue';
 
 export default {
     data() {
@@ -77,6 +91,4 @@ export default {
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
