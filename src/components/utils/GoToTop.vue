f5aab619b814f428879db6b1ccaaf5ddeb28f336
abhijeet <abhijeet3016@gmail.com>
2024-04-28 18:22:23 +0200

    add go to top button

---
+++ src/components/utils/GoToTop.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="app">
+        <button ref="scrollTopBtn" @click="scrollToTop" id="scrollToTopBtn" title="Go to top"
+            class="btn btn-lg btn-primary">
+            <i class="bi bi-arrow-up"></i>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            showButton: false,
+        };
+    },
+    mounted() {
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    beforeUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    },
+    methods: {
+        handleScroll() {
+            this.showButton = window.scrollY > 20;
+        },
+        scrollToTop() {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        },
+    },
+};
+</script>
+
+<style scoped>
+#scrollToTopBtn {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    z-index: 99;
+}
+</style>
